58d04fa637be216be8640f9a6f0fae11e793b056
enrl1495 <51463080+enrl1495@users.noreply.github.com>
2024-06-13 12:26:22 -0600

    Update pantalla.ino

--- pantalla.ino
+++ pantalla.ino
@@ -5,7 +5,9 @@ LiquidCrystal_I2C lcd (0x27, 16, 2);
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println(lcd.init());
+  delay(2000);
 }
 
 void loop() {
