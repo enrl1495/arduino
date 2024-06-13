9b002a2d52ebd144a4bd0a0f0a42a4988327bcc4
enrl1495 <51463080+enrl1495@users.noreply.github.com>
2024-06-13 12:24:35 -0600

    Create pantalla.ino

---
+++ pantalla.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd (0x27, 16, 2);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
